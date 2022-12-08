a9b77b7f57f7867c183a41cdb582f6f4409a559d
creawww <creawww@gmail.com>
2022-12-08 19:15:00 +0100

    changes ci dockerhub

--- urltomd.sh
+++ urltomd.sh
@@ -1,6 +1,7 @@
 urltomd(){
 	docker run --rm \
-        -v $(pwd):/home/myuser/app/date_ex \
-        manologcode/urltomd \
-		manologcode/terminal_util \
-		python app.py "$@"
+	-v $(pwd):/home/myuser/app/date_ex \
+	manologcode/urltomd \
+	manologcode/terminal_util \
+	python app.py "$@"
+}
